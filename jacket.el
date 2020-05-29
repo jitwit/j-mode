6b8399c1a7c69c93cab96aec84aa4f597ef352b5
jitwit <jrn@bluefarm.ca>
2020-05-29 16:01:04 -0400

    nuvoc stuff

---
+++ jacket.el
@@ -0,0 +1,56 @@
+;;; jacket.el --- Major mode for J  -*- lexical-binding: t -*-
+
+(defun the-NuVoc ()
+  (read (format "(progn %s)"
+                (with-temp-buffer
+                  (insert-file-contents "data/j.sexp")
+                  (buffer-string)))))
+
+(require 'NuVoc)
+(require 'popup)
+(require 'browse-url)
+
+(defun j-find-thing (thing)
+  "Find information about thing (exact match)"
+  (interactive "sthing: ")
+  (seq-find #'(lambda (jentity)
+                (member thing (cdadr jentity)))
+            j-nuvoc))
+
+(defun j-urls (thing)
+  "Look up urls related to a thing (exact match)"
+  (let ((entity (j-find-thing thing)))
+    (if entity
+        (seq-map #'(lambda (info)
+                     ;; guaranteed fields
+                     (append (cdr (assoc 'description (cdr info)))
+                             (cdr (assoc 'url (cdr info)))))
+                 (seq-filter #'(lambda (kv)
+                                 (equal (car kv) 'info))
+                             (cdr entity)))
+      nil)))
+
+(defun j-names (thing)
+  "Look up english names for thing"
+  (seq-map #'car (j-urls thing)))
+
+(defun joogle (thing)
+  "Present a popup with links to information about thing"
+  (interactive "sJOOGLE: ")
+  (let ((urls (seq-map #'(lambda (url)
+                           (popup-make-item (seq-elt url 0) :value (seq-elt url 1)))
+                       (j-urls thing))))
+    (when urls
+      (browse-url (popup-menu* urls)))))
+
+; (defun jacket ()
+;   "J minor mode to improve experience in emacs"
+;   (interactive)
+;   (add-hook 'j-mode-hook
+;             (lambda ()
+;               (local-set-key (kbd "C-c j") 'joogle))))
+
+;; ;;;###autoload
+;; (add-to-list 'auto-mode-alist '("\\.ij[rstp]$" . jacket))
+
+(provide 'jacket)
