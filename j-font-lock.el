82b7528a73d79d84b9f7250f50842f6490307387
Joseph Novakovich <josephnovakovich@gmail.com>
2020-09-04 20:52:51 -0400

    reinclude old copyright

--- j-font-lock.el
+++ j-font-lock.el
@@ -1,3 +1,49 @@
+;;; j-font-lock.el --- font-lock extension for j-mode  -*- lexical-binding: t -*-
+;; Copyright (C) 2012 Zachary Elliott
+;;
+;; Authors: Zachary Elliott <ZacharyElliott1@gmail.com>
+;; URL: http://github.com/zellio/j-mode
+;; Version: 1.1.1
+;; Keywords: J, Languages
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; `j-mode` font-lock provides four new faces for management of the coloring
+;; various parts of speech. Those faces are `j-verb-face` `j-adverb-face`
+;; `j-conjunction-face` `j-other-face`. They can be modified like any of the
+;; standard built in faces to help meet your need.
+;;
+;; (custom-set-face
+;;  '(j-verb-face ((t (:foreground "Red"))))
+;;  '(j-adverb-face ((t (:foreground "Green"))))
+;;  '(j-conjunction-face ((t (:foreground "Blue"))))
+;;  '(j-other-face ((t (:foreground "Black")))))
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+;;; Code:
+
+;; (defconst j-font-lock-version "1.1.1"
+;;   "`j-font-lock' version")
+
+
 (defgroup j-font-lock nil
   "font-lock extension for j-mode"
   :group 'j
@@ -61,7 +107,7 @@
   '("assert."  "break."  "continue."  "while."  "whilst."  "for."  "do."  "end."
     "if."  "else."  "elseif."  "return."  "select."  "case."  "fcase."  "throw."
     "try."  "catch."  "catchd."  "catcht."  "end."))
-    ;; "for_[a-zA-Z]+\\."  "goto_[a-zA-Z]+\\."  "label_[a-zA-Z]+\\."
+
 
 
  ; todo: negative constants eg _3:
@@ -107,8 +153,7 @@
 (defvar j-font-locks
   `((
      ;; one day: multiline strings and inline explicit defs
-     (,(rx "NB." (* not-newline))
-      . font-lock-comment-face)
+     (,(rx "NB." (* not-newline))     . font-lock-comment-face)
      
      (,(rx (or (submatch-n 1 (eval j-identifier))
 	       (seq "'" (submatch-n 1 (eval j-identifier)
@@ -118,6 +163,12 @@
 	   (submatch-n 2 (or "=." "=:")))
       (1 font-lock-variable-name-face)
       (2 j-other-face))
+     (,(rx (submatch-n 1 (or "for_" "goto_" "label_"))
+	   (submatch-n 2 (+ alpha))
+	   (submatch-n 3 "."))
+      (1 font-lock-keyword-face)
+      (2 font-lock-variable-name-face)
+      (3 font-lock-keyword-face))
      
      (,(rx "'" (* (not "'")) "'")     . font-lock-string-face)
      (,(rx (eval `(or ,@j-controls))) . font-lock-keyword-face)
